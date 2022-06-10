f6c69c7b548d1b01bf2f85bcefb8745eddf5de8d
hermanb001 <94038133+hermanb001@users.noreply.github.com>
2022-06-10 23:03:56 +0800

    Update and rename test.py to main0.py

---
+++ main0.py
@@ -0,0 +1,69 @@
+import requests
+import json
+import time
+import re
+import logging
+import traceback
+import os
+import random
+import datetime
+import utils
+# import base64
+
+
+def main(event, context):
+    # 初始化日志文件
+    utils.initLog('log.txt')
+    utils.clearLog()
+    savePoint(
+        'https://eu-proxypool.herokuapp.com/clash/proxies?nc=CN&speed=30', 'eu-proxypool')
+
+    savePoint(
+        'https://free886.herokuapp.com/clash/proxies', 'free886')
+      
+    savePoint(
+        'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang')
+
+    savePoint(
+        'https://hellopool.herokuapp.com/clash/proxies?nc=CN&speed=50', 'hellopool')
+    
+    savePoint(
+        'https://free.jingfu.cf/clash/proxies?nc=CN&speed=20', 'jingfu')
+    
+    savePoint(
+        'https://fq.lonxin.net/clash/proxies?nc+CN&type=trojan', 'lonxin')
+    
+    savePoint(
+        'https://origamiboy.herokuapp.com/clash/proxies?nc=CN&speed=10', 'origamiboy')
+    
+    savePoint(
+        'https://sspool.herokuapp.com/clash/proxies?nc=CN&speed=50', 'sspool')
+    
+#    savePoint(
+#        'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang-30')
+    
+#    savePoint(
+#        'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang-30')
+    
+#    savePoint(
+#        'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang-30')
+    
+
+# 获取文章地址
+
+
+def savePoint(url, name):
+    resp = requests.get(url)
+    dirs = './subscribe'
+    day = time.strftime('%Y.%m.%d', time.localtime(time.time()))
+    if 'proxies' in resp.text:
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+        with open(dirs + '/' + name, 'w', encoding='utf-8') as f:
+            f.write(resp.text.replace('"name":"','"name":"'+ day+'_'))
+            print(name+'生成成功')
+
+
+# 主函数入口
+if __name__ == '__main__':
+    main("", "")
