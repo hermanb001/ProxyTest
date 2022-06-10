dba4f036d4ac7bafa87585f48f2784c474b285ec
hermanb001 <94038133+hermanb001@users.noreply.github.com>
2022-06-10 22:50:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def main(event, context):
         'https://free886.herokuapp.com/clash/proxies', 'free886-00')
 
     savePoint(
-        'https://proxypool-guest997.herokuapp.com/clash/proxies?nc=CN&speed=50', 'guest997-50')
+        'https://free.jingfu.cf/clash/proxies?nc=CN&speed=20', 'guest997-50')
       
     savePoint(
         'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang-30')
