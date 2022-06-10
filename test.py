145662a8b5fbe8107a6dc66a2ce28667a7fc19c1
hermanb001 <94038133+hermanb001@users.noreply.github.com>
2022-06-10 22:41:43 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import time
+import re
+import logging
+import traceback
+import os
+import random
+import datetime
+import utils
+# import base64
+
+
+def main(event, context):
+    # 初始化日志文件
+    utils.initLog('log.txt')
+    utils.clearLog()
+    savePoint(
+        'https://free886.herokuapp.com/clash/proxies', 'free886-00')
+
+     savePoint(
+        'https://proxypool-guest997.herokuapp.com/clash/proxies?nc=CN&speed=50', 'guest997-50')
+      
+    savePoint(
+        'http://guobang.herokuapp.com/clash/proxies?nc=CN&speed=30', 'guobang-30')
+
+
+# 获取文章地址
+
+
+def savePoint(url, name):
+    resp = requests.get(url)
+    dirs = './subscribe'
+    day = time.strftime('%Y.%m.%d', time.localtime(time.time()))
+    if 'proxies' in resp.text:
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+        with open(dirs + '/' + name, 'w', encoding='utf-8') as f:
+            f.write(resp.text.replace('"name":"','"name":"'+ day+'_'))
+            print(name+'生成成功')
+
+
+# 主函数入口
+if __name__ == '__main__':
+    main("", "")
